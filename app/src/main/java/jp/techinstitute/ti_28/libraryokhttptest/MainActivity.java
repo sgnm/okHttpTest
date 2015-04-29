52d23593d152bbdc69e137226c4860caffc2981b
sgnm <suga315shin@gmail.com>
2015-04-29 10:30:13 +0900

    logic部分

--- app/src/main/java/jp/techinstitute/ti_28/libraryokhttptest/MainActivity.java
+++ app/src/main/java/jp/techinstitute/ti_28/libraryokhttptest/MainActivity.java
@@ -2,8 +2,17 @@ package jp.techinstitute.ti_28.libraryokhttptest;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +21,30 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void onClickDownloadButton() {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://tomorrowkey.github.io/AndroidOpenTextBook/sample.json")
+                .addHeader("Host", "tomorrowkey.github.io")
+                .get().build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Toast.makeText(getApplicationContext(), "failure", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                Log.d("TEST", "Status-Code" + response.code());
+                Log.d("TEST", "Content-Length" + response.header("Content-Length"));
+                Log.d("TEST", "Content-Type" + response.header("Content-TYpe"));
+                Log.d("TEST", response.body().toString());
+            }
+        });
     }
 
 
